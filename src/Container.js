3c6ba951a2e4b170b0e07ee66d7a32f5160b7a1f
angelonimartin90 <angelonimartin90@gmail.com>
2019-08-15 19:56:22 -0300

    Container started

--- src/Container.js
+++ src/Container.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './Container.scss';
 
 function Container() {
   return(
     <div className="increment-decrement-container">
-      <div className="number-show">HOLAAAA</div>
-      <button className="button--increment"></button>
-      <button className="button--decrement"></button>
+      <div>
+        <div className="number-show">This is an example of Redux with increment and decrement</div>
+        <label id="showLabel">Do a click in increment button</label>
+        <div className="button-container-center">
+          <div className="button-container-row">
+            <button className="button-increment">Decrement</button>
+            <button className="button-decrement">Increment</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
