6544af23b8be7e7db217f9942e5140b7f7f6111c
angelonimartin90 <angelonimartin90@gmail.com>
2019-08-15 20:47:00 -0300

    Provider added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Container from './Container';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<Container />, document.getElementById('root'));
+
+const counterReducer = (state=0, action) => {
+    if (action.type === 'INCREMENT') {
+        return state + 1;
+    }
+    if (action.type === 'DECREMENT') {
+        return state - 1;
+    }
+}
+
+let store = createStore(counterReducer);
+console.log(store.getState());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Container />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
