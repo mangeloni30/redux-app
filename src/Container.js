5d8836552059123fee4da0b7c3eba66fbaac45ff
angelonimartin90 <angelonimartin90@gmail.com>
2019-08-15 19:16:15 -0300

    container component created

---
+++ src/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Container() {
+  return(
+    <div className="increment-decrement-container">
+      <div className="number-show">HOLAAAA</div>
+      <button className="button--increment"></button>
+      <button className="button--decrement"></button>
+    </div>
+  )
+}
+
+export default Container;
